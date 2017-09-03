0c2caa71a85f7f572b82c08905769de24492e966
rgocal <rgocal09@gmail.com>
2017-09-03 10:15:34 -0400

    Initial Push

---
+++ app/src/main/java/com/gsd/sapphyxapi/Constants.java
@@ -0,0 +1,7 @@
+package com.gsd.sapphyxapi;
+
+public class Constants {
+
+    public static final String LAUNCHER_PACKAGE_NAME = "com.gsd.launcher.sapphyx";
+
+}
---
+++ app/src/main/java/com/gsd/sapphyxapi/SettingsKeys.java
@@ -0,0 +1,18 @@
+package com.gsd.sapphyxapi;
+
+/**
+ * Created by ry on 9/3/17.
+ * SharedPreferences used in Sapphyx Launcher to allow users some Color Options in Pages.
+ * That is if the dev adds color options...
+ * This is only necessary if the dev does not add a settings page to their page or project.
+ * We can add more at anytime!
+ */
+
+public class SettingsKeys {
+
+    String COLOR_PAGE_PRIMARY = "pref_staticPrimary";
+    String COLOR_PAGE_PRIMARYDARK = "pref_staticPrimaryDark";
+    String COLOR_PAGE_ACCENT = "pref_staticAccent";
+    String COLOR_PAGE_BACKGROUND = "pref_staticBackground";
+
+}
